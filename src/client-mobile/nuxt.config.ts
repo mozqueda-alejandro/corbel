d3edb6ee0e38232cbc8a77536271d8d698aacea3
mozqueda-alejandro <mozqueda.am@gmail.com>
2026-08-15 18:20:02 -0700

    chore: update lockfile

--- src/client-mobile/nuxt.config.ts
+++ src/client-mobile/nuxt.config.ts
@@ -1,28 +1,28 @@
 export default defineNuxtConfig({
   modules: [
-    '@nuxt/eslint',
-    '@nuxt/ui'
+    "@nuxt/eslint",
+    "@nuxt/ui"
   ],
+  ssr: false,
   devtools: {
     enabled: true
   },
-  css: ['~/assets/css/main.css'],
-  routeRules: {
-    '/': { prerender: true }
+  css: ["~/assets/css/main.css"],
+  runtimeConfig: {
+    public: {
+      // apiBaseUrl: process.env.NUXT_PUBLIC_API_BASE_URL
+    }
   },
-  compatibilityDate: '2025-01-15',
+  routeRules: {
+    "/": { prerender: true }
+  },
+  compatibilityDate: "2025-01-15",
   eslint: {
     config: {
       stylistic: {
-        commaDangle: 'never',
-        braceStyle: '1tbs'
+        commaDangle: "never",
+        braceStyle: "1tbs"
       }
     }
-  },
-  runtimeConfig: {
-    public: {
-      apiBaseUrl: process.env.NUXT_PUBLIC_API_BASE_URL
-    }
-  },
-  ssr: false
-})
+  }
+});
