facbb5d1bab3aac53dc5b6188e8441514632af9c
mozqueda-alejandro <mozqueda.am@gmail.com>
2026-09-09 01:48:32 -0700

    chore: DBTest

--- src/client-mobile/app/composables/useDBTest.ts
+++ src/client-mobile/app/composables/useDBTest.ts
@@ -1,8 +1,10 @@
 export function useDBTest() {
   async function clearUserAndClassData() {
     const database = useDatabase();
+    await database.sessionTable.clear();
     await database.userTable.clear();
     await database.classTable.clear();
+    await database.noteTable.clear();
     window.location.reload();
   }
 
